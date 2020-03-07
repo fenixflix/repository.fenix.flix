0fd3dc97f8528fceaab55e5d792d11150f9353cb
Fênix Flix <44162234+fenixflix@users.noreply.github.com>
2020-03-07 10:24:10 -0300

    Update

--- Postagens/script-postagem-fenix.py
+++ Postagens/script-postagem-fenix.py
@@ -7,35 +7,9 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-def debrid_format(link):
-    print("-------------------------------------------")
-    print("--------Digite 1 Google Drive -------------")
-    print("--------Digite 2 Youtube ------------------")
-    #print("--------Digite 3 Dailymotion --------------")
-    #print("--------Digite 4 Playthis -----------------")
-    #print("--------Digite 5 Elementum -----------------")
-    print("\n\n")
-
-    opcao = input('Digite uma opção: ')
-    if opcao == '1':
-        link2 ="plugin://plugin.video.gdrive?mode=streamURL&amp;url="+link
-        return link2
-    if opcao == '2':
-        link2 = "plugin://plugin.video.youtube/?action=play_video&amp;videoid="+link
-        return link2
-    if opcao == '4':
-        link2 = "plugin://plugin.video.playthis/?mode=play&player=false&path="+link
-        return link2
-    if opcao == '5':
-        link2 = "plugin://plugin.video.elementum/play?uri="+link
-        return link2
-
-ids = input('Digite o codigo do Filme na TMDB: ')
+ids = input('Digite o codigo do Filme do TMDB e de Enter: ')
 ids2 = str(ids)
-
-link = input('Digite o Link do Filme: ')
-t5 = debrid_format(link)
-
+#link = input('Digite o Link do Filme do GDrive e de Enter: ')
 
 headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
 url = "https://www.themoviedb.org/movie/"+ids2+"?language=pt-BR"
@@ -60,14 +34,9 @@ t2 = sinopse2.replace('[<meta content="','').replace('" property="og:description
 t3 = thumb2.replace('[<meta content="','').replace('" property="og:image"/>]','').replace('" property="og:image"/>, <meta content="','\n').replace('','')
 t4 = fanart4.replace('data-src="','')
 
-print("<channels></channels>")
-print("<channel>")
-print("<name>"+t1+"</name>")
-print("<thumbnail>"+t3+"</thumbnail>")
 print("\n")
 print("<item>")
 print("<title>[B]"+t1+" l [COLOR orange]IMDb XX[/COLOR] l[/B]</title>")
-print("<link>"+t5+"</link>")
 print("<link>plugin://plugin.video.youtube/play/?video_id=$texto=Trailer</link>")
 print("<link>plugin://plugin.video.gdrive?mode=streamURL&amp;url=https://drive.google.com/open?id=$texto=Filme</link>")
 print("<thumbnail>"+t3+"</thumbnail>")
@@ -76,11 +45,3 @@ print("<info>[B][COLOR firebrick]l[/COLOR][/B] "+t2+" [B][COLOR firebrick]l[/COL
 print("<genre>""</genre>")
 print("</item>")
 print("\n")
-print("<title>Trailer</title>")
-print("<trailer>plugin://plugin.video.youtube/?action=play_video&amp;videoid=</trailer>")
-print("<thumbnail></thumbnail>")
-print("<fanart></fanart>")
-print("<info></info>")
-print("</item>")
-print("</channel>")
-print("\n\n")
